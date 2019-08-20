7c61783e9d781975b6b3036bd7f8366340516e67
Cai Jenkins <Caisjenkins@gmail.com>
2019-08-20 15:48:52 +0100

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import '../styles/NavBar.css';
+
+const NavBar = <i className="fas fa-igloo"/> "Surreal Estate" </i>;
+
+export default NavBar;
---
+++ src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+import '../styles/app.css';
+
+const App = <div> Hello world</div>;
+
+
+export default App;
