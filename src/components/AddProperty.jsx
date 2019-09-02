999f34cb505ceca5b835ad0167a2cb69b4aaaf92
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-02 18:40:49 +0100

    added state

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -1,8 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 
-const AddProperty = () => (
-  <Fragment>
+import '../styles/AddProperty.css';
+
+class AddProperty extends Component {
+  this.handleAddProperty = this.handleAddProperty.bind(this);
+
+  handleAddProperty = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    <div className="AddProperty">
+      <form onSubmit={this.handleAddProperty}></form>
         Add Property Page
-  </Fragment>
-);
+    </div>;
+  }
+}
+
 export default AddProperty;
