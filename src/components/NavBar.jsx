7e52314b0a94016be0488d6724179175db2c2025
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-02 15:25:26 +0100

    added pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/NavBar.css';
 
-const NavBar = props => (
+const NavBar = () => (
   <div className="NavBar">
     <img src="https://cdn1.iconfinder.com/data/icons/navigation-7/32/house-01-512.png" width="90" height="90" />
     <span>Surreal Estate</span>
     <ul className="nav">
-      <li>View Properties</li>
-      <li>Add a Property</li>
+      <Link className="item" to="/">View Properties</Link>
+      <Link className="item" to="/add-property">Add a Property</Link>
     </ul>
   </div>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './components/app';
 
-render(<App />, document.getElementById('root'));
+const app = (
+  <Router>
+    <App />
+  </Router>
+);
+
+render(app, document.getElementById('root'));
