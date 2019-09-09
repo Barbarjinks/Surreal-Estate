2a2901bf4644fbc1be77d37c0b48b5fa38cd9bcc
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-09 14:13:52 +0100

    Added css

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,12 +1,37 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import axios from 'axios';
 import PropertyCard from '../components/PropertyCard';
 
 class Properties extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/v1/PropertyListing')
+      .then(({ data }) => this.setState({ properties: data }));
+  }
+
   render() {
     return (
-      <div>
-        <PropertyCard />
+      <div className="properties">
+        {this.state.properties.map(property => (
+          <div key={property._id} className="col">
+            <PropertyCard
+              key={property._id}
+              price={property.price}
+              title={property.title}
+              bathrooms={property.bathrooms}
+              bedrooms={property.bedrooms}
+              type={property.type}
+              email={property.email}
+              city={property.city}
+            />
+          </div>
+        ))}
       </div>
     );
   }
--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -8,11 +8,11 @@ const PropertyCard = props => {
       <div id="picture">
         <img id="house" src="https://cdn1.iconfinder.com/data/icons/navigation-7/32/house-01-512.png" width="60" height="60" alt="house" />
       </div>
-      <span>£{props.price}</span>
-      <br />
       <span>{props.type}</span>
       <br />
-      <span>{props.city}</span>
+      <span>£{props.price}</span>
+      <br />
+      <span>Location:{props.city}</span>
       <br />
       <span>Bedrooms: {props.bedrooms}</span>
       <br />
