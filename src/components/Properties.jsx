c4c08b1b8c56997059d0321687059578ad6c5b1f
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-09 21:05:51 +0100

    added filter function

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import PropertyCard from '../components/PropertyCard';
+import { Link } from 'react-router-dom';
 
 class Properties extends React.Component {
   constructor(props) {
@@ -15,9 +16,25 @@ class Properties extends React.Component {
       .then(({ data }) => this.setState({ properties: data }));
   }
 
+  componentDidUpdate(prevProps) {
+    const { search } = this.props.location;
+
+    if (prevProps.location.search !== search) {
+      axios.get(`http://localhost:3000/api/v1/PropertyListing${this.props.location.search}`)
+        .then(({ data: properties }) => this.setState({ properties }))
+        .catch(err => console.error(err));
+    }
+  }
+
   render() {
     return (
       <div className="properties">
+        <div className="sidebar">
+          <Link to={'/?query={"city": "Manchester"}'}>Manchester</Link>
+          <Link to={'/?query={"city": "Leeds"}'}>Leeds</Link>
+          <Link to={'/?query={"city": "Sheffield"}'}>Sheffield</Link>
+          <Link to={'/?query={"city": "Liverpool"}'}>Liverpool</Link>
+        </div>
         {this.state.properties.map(property => (
           <div key={property._id} className="col">
             <PropertyCard
